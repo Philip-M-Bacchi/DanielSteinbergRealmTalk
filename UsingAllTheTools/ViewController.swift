2ffc2dc62485da7de6c2f50a910905cd9b65c2ed
swillsea <sam.willsea@gmail.com>
2016-10-14 18:45:24 -0700

    Initial Commit

---
+++ UsingAllTheTools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingAllTheTools
+//
+//  Created by Sam on 10/14/16.
+//  Copyright © 2016 SWillsea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
